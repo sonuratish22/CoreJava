07f5bfd60b9c1de30ad82b6a7d4ffda9baedfed1
Kumar <Sonu.Kumar@aristocrat.com>
2022-06-23 11:33:56 +0530

    added class file

---
+++ src/main/java/Array/ArrayConcept.java
@@ -0,0 +1,8 @@
+package Array;
+
+public class ArrayConcept {
+
+    public static void main(String[] args) {
+        System.out.println("Lets start with Array Concept");
+    }
+}
--- src/main/java/OOPS/Polymorphism.java
+++ src/main/java/OOPS/Polymorphism.java
@@ -1,4 +1,8 @@
 package OOPS;
 
 public class Polymorphism {
+
+    public static void main(String[] args) {
+        System.out.println("Hello my first code");
+    }
 }
